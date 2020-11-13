1a5fe0ff91b85e58ce1734cab43c37b4118c6978
Daniel Kamraš <dani023zr@gmail.com>
2020-11-13 19:28:09 +0100

    Set up routing support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '../src/theme';
+import { createMemoryHistory } from 'history';
+import { Switch, Router, Route } from 'react-router-dom';
+
+const history = createMemoryHistory();
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <Story />
+      <Router history={history}>
+        <Switch>
+          <Route path="/route1" component={Story} />
+          <Route path="/route2" component={Story} />
+          <Route path="/route3" component={Story} />
+          <Route path="/" component={Story} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   ),
 ];
--- src/utils/renderTestComponent.tsx
+++ src/utils/renderTestComponent.tsx
@@ -2,9 +2,15 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { render, RenderResult } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
 import theme from '../theme';
 
 const renderTestElement = (element: JSX.Element): RenderResult =>
-  render(<ThemeProvider theme={theme}>{element}</ThemeProvider>);
+  render(
+    <ThemeProvider theme={theme}>
+      <Router history={createMemoryHistory()}>{element}</Router>
+    </ThemeProvider>
+  );
 
 export default renderTestElement;
