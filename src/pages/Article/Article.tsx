51f431e904645069be915c8d93495acf4132271b
Daniel Kamraš <dani023zr@gmail.com>
2020-11-14 23:21:46 +0100

    Add basic article page

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import articles from '../../components/ArticlesGrid/mock-data/articles.mock';
+import ArticleComponent from '../../components/Article';
 
-const Article: React.FC = () => <></>;
+const Article: React.FC = () => {
+  const article = articles[0];
+
+  return (
+    <ArticleComponent
+      title={article.title}
+      imageSrc={article.urlToImage}
+      description={article.description}
+      content={article.content}
+    />
+  );
+};
 
 export default Article;
